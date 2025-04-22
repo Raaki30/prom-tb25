a58c23c66ad36ccd9c3b78ada2f6be74db63a263
Raaki30 <rafifradina@gmail.com>
2025-04-22 21:19:59 +0700

    update vite again

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
 export default defineConfig({
-  server: {
-    host: true,
-    hmr: {
-      host: 'promtb-25-k95ii.ondigitalocean.app',
-      protocol: 'wss', // ✅ Wajib untuk HTTPS
-      port: 443, // ✅ Port standar HTTPS
-    },
-    watch: {
-      usePolling: true // ✅ Penting jika pakai Docker/WSL
-    }
-  },
   plugins: [
-    laravel([
-      'resources/css/app.css',
-      'resources/js/app.js',
-    ]),
+    laravel({
+      input: ['resources/css/app.css', 'resources/js/app.js'],
+      refresh: true,
+    }),
   ],
+  build: {
+    manifest: true,
+    outDir: 'public/build',
+  },
+  base: process.env.ASSET_URL || '/',
 });
